af6d0a0a11fb71b6af67859d95ed0a0425f43948
zcong1993 <zhangcong1992@gmail.com>
2020-12-08 18:12:05 +0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{crate_version, App, Arg};
 use regex::Regex;
-use std::{collections, env, error::Error, path, thread};
-use subprocess::Exec;
+use std::{collections, env, path, thread};
+use subprocess::{Exec, Redirection};
 
 #[derive(Debug)]
 struct Opt {
@@ -11,23 +11,33 @@ struct Opt {
 }
 
 fn get_changed_and_filter(cwd: &path::Path, args: &[&str], re: &Option<Regex>) -> Vec<String> {
-    let files: Vec<String> = Exec::cmd("git")
+    let capture = Exec::cmd("git")
         .args(args)
         .cwd(cwd)
-        .capture()
-        .unwrap()
-        .stdout_str()
-        .split("\n")
-        .filter(|x| {
-            let r = re.clone();
-            match r {
-                Some(re) => re.is_match(x),
-                None => true,
-            }
-        })
-        .map(|x| cwd.join(x).as_path().to_str().unwrap().to_string())
-        .collect();
-    files
+        .stderr(Redirection::None)
+        .capture();
+
+    match capture {
+        Ok(capture) => capture
+            .stdout_str()
+            .split("\n")
+            .filter(|&x| {
+                if x == "" {
+                    return false;
+                }
+                let r = re.clone();
+                match r {
+                    Some(re) => re.is_match(x),
+                    None => true,
+                }
+            })
+            .map(|x| cwd.join(x).as_path().to_str().unwrap().to_string())
+            .collect(),
+        Err(e) => {
+            println!("run error {}", e);
+            return vec![];
+        }
+    }
 }
 
 fn combine_unique(vecs: Vec<Vec<String>>) -> Vec<String> {
@@ -43,15 +53,69 @@ fn combine_unique(vecs: Vec<Vec<String>>) -> Vec<String> {
 }
 
 fn find_changed_files(cwd: &path::PathBuf, opt: &Opt, reg: &Option<Regex>) -> Vec<String> {
+    if opt.changed_since.is_none() && !opt.last_commit && !opt.with_ancestor {
+        let c1 = cwd.clone();
+        let c2 = cwd.clone();
+        let r1 = reg.clone();
+        let r2 = reg.clone();
+        let staged_t = thread::spawn(move || {
+            get_changed_and_filter(c1.as_path(), &["diff", "--cached", "--name-only"], &r1)
+        });
+        let unstaged_t = thread::spawn(move || {
+            get_changed_and_filter(
+                c2.as_path(),
+                &["ls-files", "--other", "--modified", "--exclude-standard"],
+                &r2,
+            )
+        });
+
+        return combine_unique(vec![staged_t.join().unwrap(), unstaged_t.join().unwrap()]);
+    }
+
     if opt.last_commit {
-        get_changed_and_filter(
+        return get_changed_and_filter(
             cwd,
             &["show", "--name-only", "--pretty=format:", "HEAD"],
             reg,
-        )
-    } else {
-        vec![]
+        );
     }
+
+    let c1 = cwd.clone();
+    let c2 = cwd.clone();
+    let c3 = cwd.clone();
+    let r1 = reg.clone();
+    let r2 = reg.clone();
+    let r3 = reg.clone();
+
+    let changed_since = opt.changed_since.clone().unwrap_or("HAED^".to_string());
+
+    let committed_t = thread::spawn(move || {
+        get_changed_and_filter(
+            c1.as_path(),
+            &[
+                "diff",
+                "--name-only",
+                format!("{}...HEAD", changed_since.as_str()).as_str(),
+            ],
+            &r1,
+        )
+    });
+    let staged_t = thread::spawn(move || {
+        get_changed_and_filter(c2.as_path(), &["diff", "--cached", "--name-only"], &r2)
+    });
+    let unstaged_t = thread::spawn(move || {
+        get_changed_and_filter(
+            c3.as_path(),
+            &["ls-files", "--other", "--modified", "--exclude-standard"],
+            &r3,
+        )
+    });
+
+    combine_unique(vec![
+        committed_t.join().unwrap(),
+        staged_t.join().unwrap(),
+        unstaged_t.join().unwrap(),
+    ])
 }
 
 fn main() {
@@ -97,12 +161,28 @@ fn main() {
         changed_since: matches.value_of("changed_since").map(|x| x.to_string()),
     };
 
-    let command = matches.value_of("command").unwrap_or("");
+    let command = matches.value_of("command");
     let reg = matches.value_of("filter").map(|x| Regex::new(x).unwrap());
     let cwd = env::current_dir().unwrap();
 
-    let res = find_changed_files(&cwd, o, &reg);
+    let mut res = find_changed_files(&cwd, o, &reg);
 
-    println!("o: {:?}", o);
-    println!("command: {:?}", res);
+    if matches.is_present("folder") {
+        res = res
+            .into_iter()
+            .map(|x| {
+                x.replace(
+                    path::Path::new(&x).file_name().unwrap().to_str().unwrap(),
+                    "",
+                )
+            })
+            .collect();
+    }
+
+    let files_str = res.join(" ");
+
+    match command {
+        None => println!("{}", files_str),
+        Some(command) => println!("{} {}", command, files_str),
+    }
 }
